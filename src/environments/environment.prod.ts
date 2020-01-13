cbcc352ba6a9be4a1295388129cacc80465010be
Daniel S. Boniface <s1087717@monmouth.edu>
2020-01-13 09:52:49 -0500

    final deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA151tYy8FMOoNdJaiwXU-WubM6_8s7mNs",
+    authDomain: "clientpanelprod-17bb8.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-17bb8.firebaseio.com",
+    projectId: "clientpanelprod-17bb8",
+    storageBucket: "clientpanelprod-17bb8.appspot.com",
+    messagingSenderId: "983070436100",
+    appId: "1:983070436100:web:499cdddb5b819d0f9e3e51",
+    measurementId: "G-TC0RG95EYH"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA151tYy8FMOoNdJaiwXU-WubM6_8s7mNs",
+    authDomain: "clientpanelprod-17bb8.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-17bb8.firebaseio.com",
+    projectId: "clientpanelprod-17bb8",
+    storageBucket: "clientpanelprod-17bb8.appspot.com",
+    messagingSenderId: "983070436100",
+    appId: "1:983070436100:web:499cdddb5b819d0f9e3e51",
+    measurementId: "G-TC0RG95EYH"
+  }
 };
 
 /*
